c77354f288a60e883b61c00384fb7bb37aa920ff
Marek Śliwiński <msliwinski@onet.pl>
2017-10-05 17:47:46 +0200

    Dodanie pliku z kodem

--- src/pl/mareksliwinski/Tab.java
+++ src/pl/mareksliwinski/Tab.java
@@ -1,8 +1,35 @@
 package pl.mareksliwinski;
+import java.util.Arrays;
+import static java.util.Comparator.reverseOrder;
 
 public class Tab {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+public void setup()
+{
+    String text = ("Ala ma wielkiego kota");
+    int max = text.length() + 1;
+    int[] tab = new int[max];
+    System.out.println(tab.length);
 }
+
+    public void fillOut(int[] tab, int max) {
+
+        for (int i = max; i > 0; i--) {
+            tab[i] = i * i;
+        }
+    }
+
+
+    /*public void sort(){
+        Arrays.sort(tab);
+        for(int i = 0; i < max; i++){
+            System.out.println(tab[i]);
+        }*/
+        //Arrays.sort(tab, reverseOrder());
+        //for(int i = 0; i < max; i++){
+          //  System.out.println(tab[i]);
+}
+
+
+
+
