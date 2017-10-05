eb9cd50ed76387096189da926c75684bd4e2ba74
Marek Śliwiński <msliwinski@onet.pl>
2017-10-05 23:06:44 +0200

    Zmiana typu zmiennnej max

--- src/pl/mareksliwinski/Tab.java
+++ src/pl/mareksliwinski/Tab.java
@@ -4,16 +4,16 @@ import java.util.Collections;
 
 public class Tab {
 
-    Integer max;
+    int max;
     Integer tab[];
 
     public void setup() {
         String text = ("Ala ma wielkiego");
-        max = text.length() + 1;
+        max = text.length();
         tab = new Integer[max];
     }
 
-    public void fillOut(Integer[] tab, Integer max) {
+    public void fillOut(Integer[] tab, int max) {
         for (int i = 0; i < max; i++) {
             tab[i] = (int) (Math.random() * 100);
             System.out.println(tab[i]);
